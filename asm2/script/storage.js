6d1c5cca98085f56e4f4de92d241d22c992fbfb7
vanlam6061 <119390647+vanlam6061@users.noreply.github.com>
2022-12-29 10:36:59 +0700

    Add files via upload

---
+++ asm2/script/storage.js
@@ -0,0 +1,45 @@
+"use strict";
+function saveToStorage(key, value) {
+  localStorage.setItem(key, value);
+}
+function getFromStorage(key) {
+  return localStorage.getItem(key);
+}
+let localPetArr = [
+  {
+    id: "P001",
+    name: "Tom",
+    age: 3,
+    type: "Cat",
+    weight: 5,
+    lengthPet: 50,
+    color: "#ff0000",
+    breed: "Tabby",
+    vaccinated: true,
+    dewormed: true,
+    sterilized: true,
+    bmi: null,
+    date: "01/03/2022",
+  },
+  {
+    id: "P002",
+    name: "Type",
+    age: 5,
+    type: "Dog",
+    weight: 3,
+    lengthPet: 40,
+    color: "#008000",
+    breed: "Mixed Breed",
+    vaccinated: false,
+    dewormed: false,
+    sterilized: false,
+    bmi: null,
+    date: "02/03/2022",
+  },
+];
+saveToStorage("localPetArr", JSON.stringify(localPetArr));
+console.log(localPetArr);
+//locaBreedArr storage 
+let localBreedArr = [];
+saveToStorage("localBreedArr", JSON.stringify(localBreedArr));
+console.log(localBreedArr);
