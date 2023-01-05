884bf0e3c25f16dfa9d48a0e1e7e3874c3d9b4e2
vanlam6061 <119390647+vanlam6061@users.noreply.github.com>
2023-01-05 17:00:54 +0700

    Add files via upload

--- script/edit.js
+++ script/edit.js
@@ -1,8 +1,10 @@
 "use strict";
 // Lấy dữ liệu từ local và hiển thị
 const editPetArr = JSON.parse(getFromStorage("localPetArr"));
+console.log(editPetArr);
+const editTable = document.getElementById("tbody");
 function renderEditTableData(petArr) {
-  tableBodyEl.innerHTML = "";
+  editTable.innerHTML = "";
   for (let i = 0; i < petArr.length; i++) {
     const row = document.createElement("tr");
     row.innerHTML = `
@@ -25,14 +27,13 @@ function renderEditTableData(petArr) {
 		<td><i class="bi ${
       petArr[i].sterilized == true ? "bi-check-circle-fill" : "bi-x-circle-fill"
     }"></i></td>
-    <td>${petArr[i].bmi ?? "?"} </td>
 		<td>${petArr[i].date}</td>
 		<td><button type="button" class="btn btn-danger" onclick="editPet('${
       petArr[i].id
     }')">Edit</button>
 		</td>
   `;
-    tableBodyEl.appendChild(row);
+    editTable.appendChild(row);
   }
 }
 renderEditTableData(editPetArr);
@@ -56,14 +57,14 @@ const editPet = function () {
 								<label for="input-age" class="col-sm-1 col-form-label"
 									style="text-align:right">Age</label>
 								<div class="col-sm-3">
-									<input type="number" class="form-control" id="input-age" placeholder="Input Age">
+									<input type="number" class="form-control" id="input-age" placeholder="Input Age">${petArr[i].age}
 								</div>
 							</div>
 							<div class="form-group row mb-3">
 								<label for="input-type" class="col-sm-3 col-form-label">Type</label>
 								<div class="col-sm-9">
 									<select class="form-control" id="input-type">
-										<option>Select Type</option>
+										<option>${petArr[i].type}</option>
 										<option>Dog</option>
 										<option>Cat</option>
 									</select>
@@ -73,14 +74,14 @@ const editPet = function () {
 								<label for="input-weight" class="col-sm-3 col-form-label">Weight</label>
 								<div class="col-sm-3">
 									<input type="number" class="form-control" id="input-weight"
-										placeholder="Input Weight">
+										placeholder="Input Weight">${petArr[i].weight}
 								</div>
 
 								<label for="input-length" class="col-sm-3 col-form-label"
 									style="text-align:right">Length</label>
 								<div class="col-sm-3">
 									<input type="number" class="form-control" id="input-length"
-										placeholder="Input Length">
+										placeholder="Input Length">${petArr[i].lengthPeter}
 								</div>
 							</div>
 							<div class="form-group row mb-3">
@@ -92,7 +93,7 @@ const editPet = function () {
 									style="text-align:right">Breed</label>
 								<div class="col-sm-3">
 									<select class="form-control" id="input-breed">
-										<option>Select Breed</option>
+										<option>${petArr[i].breed}</option>
 									</select>
 								</div>
 							</div>
