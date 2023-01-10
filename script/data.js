f188c0cde3f04cb8a069db77719879f932ebc173
vanlam6061 <119390647+vanlam6061@users.noreply.github.com>
2023-01-10 07:44:14 +0700

    Add files via upload

--- script/data.js
+++ script/data.js
@@ -1,1 +1,35 @@
-'use strict';
+"use strict";
+const dataExport = JSON.stringify(getFromStorage("localPetArr"));
+function saveStaticDataToFile() {
+  var blob = new Blob([dataExport], {
+    type: "text/plain;charset=utf-8",
+  });
+  saveAs(blob, "exportData.txt");
+}
+
+function loadDataFromFile() {
+  try {
+    let fso = new ActiveXObject("Scripting.FileSystemObject");
+    let fh = fso.OpenTextFile(filename, 1);
+    let contents = fh.ReadAll();
+    fh.Close();
+    return contents;
+  } catch (Exception) {
+    return "Cannot open file :(";
+  }
+}
+
+// function getFileContents() {
+//   let fileForUpload = document.forms[0].fileForUpload;
+//   let fileName = fileForUpload.value;
+
+//   if (fileForUpload.files) {
+//     let fileContents = fileForUpload.files.item(0).getAsBinary();
+//     document.forms[0].fileContents.innerHTML = fileContents;
+//   } else {
+//     // try the IE method
+//     let fileContents = ieReadFile(fileName);
+//     document.forms[0].fileContents.innerHTML = fileContents;
+//   }
+// }
+// getFileContents();
